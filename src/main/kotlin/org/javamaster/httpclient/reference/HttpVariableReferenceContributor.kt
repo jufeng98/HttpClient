2079c519e75771e75f61eea27fe09ad2c2bd298c
jufeng98 <13412699@Yu$$$>
2024-10-16 20:57:02 +0800

    转换为kotlin文件

--- src/main/kotlin/org/javamaster/httpclient/reference/HttpVariableReferenceContributor.kt
+++ src/main/kotlin/org/javamaster/httpclient/reference/HttpVariableReferenceContributor.kt
@@ -1,23 +1,27 @@
-package org.javamaster.httpclient.reference;
+package org.javamaster.httpclient.reference
 
-import com.intellij.patterns.PlatformPatterns;
-import com.intellij.psi.PsiReferenceContributor;
-import com.intellij.psi.PsiReferenceRegistrar;
-import org.javamaster.httpclient.psi.HttpHeaders;
-import org.javamaster.httpclient.psi.HttpUrl;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiReferenceContributor
+import com.intellij.psi.PsiReferenceRegistrar
+import org.javamaster.httpclient.psi.HttpHeaders
+import org.javamaster.httpclient.psi.HttpUrl
 
 /**
  * @author yudong
  */
-public class HttpVariableReferenceContributor extends PsiReferenceContributor {
+class HttpVariableReferenceContributor : PsiReferenceContributor() {
+    override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
+        val provider = HttpVariablePsiReferenceProvider()
 
-    @Override
-    public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
-        HttpVariablePsiReferenceProvider provider = new HttpVariablePsiReferenceProvider();
-
-        registrar.registerReferenceProvider(PlatformPatterns.psiElement(HttpUrl.class), provider);
-        registrar.registerReferenceProvider(PlatformPatterns.psiElement(HttpHeaders.class), provider);
+        registrar.registerReferenceProvider(
+            PlatformPatterns.psiElement(
+                HttpUrl::class.java
+            ), provider
+        )
+        registrar.registerReferenceProvider(
+            PlatformPatterns.psiElement(
+                HttpHeaders::class.java
+            ), provider
+        )
     }
-
 }
