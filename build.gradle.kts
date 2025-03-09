d71c92c5cdba3fb6d74488c04f11f3d12f24a84e
liangyudong <liangyudong@bluemoon.com.cn>
2025-03-09 21:05:17 +0800

    调整实时模板

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,12 +42,12 @@ intellij {
 
 tasks {
     withType<JavaCompile> {
-        sourceCompatibility = "21"
-        targetCompatibility = "21"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
         options.encoding = "UTF-8"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "21"
+        kotlinOptions.jvmTarget = "17"
     }
 
     jar {
