897d38f8e500bd6a74c5058b36986374b0adb000
liangyudong <liangyudong@bluemoon.com.cn>
2025-03-19 17:38:08 +0800

    新增js require函数路径跳转功能

---
+++ src/main/kotlin/org/javamaster/httpclient/reference/JsFilePathGotoDeclarationHandler.kt
@@ -0,0 +1,56 @@
+package org.javamaster.httpclient.reference
+
+import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandler
+import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.elementType
+import org.javamaster.httpclient.psi.HttpScriptBody
+import org.javamaster.httpclient.utils.HttpUtils
+
+/**
+ * @author yudong
+ */
+class JsFilePathGotoDeclarationHandler : GotoDeclarationHandler {
+
+    override fun getGotoDeclarationTargets(
+        element: PsiElement?,
+        offset: Int,
+        editor: Editor?,
+    ): Array<PsiElement> {
+        if (element == null) {
+            return arrayOf()
+        }
+
+        val project = element.project
+
+        val injectionHost = InjectedLanguageManager.getInstance(project).getInjectionHost(element)
+        if (injectionHost !is HttpScriptBody) {
+            return arrayOf()
+        }
+
+        if (element.elementType.toString() != "StringLiteral") {
+            return arrayOf()
+        }
+
+        val text = element.text
+        if (text.length < 3) {
+            return arrayOf()
+        }
+
+        val path = text.substring(1, text.length - 1)
+
+        try {
+            val httpFileParentPath = (injectionHost as HttpScriptBody).containingFile.virtualFile.parent.path
+
+            val item = HttpUtils.resolveFilePath(path, httpFileParentPath, project) ?: return arrayOf()
+
+            return arrayOf(item)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return arrayOf()
+    }
+
+}
---
+++ src/main/kotlin/org/javamaster/httpclient/reference/support/WebCalm.kt
@@ -0,0 +1,73 @@
+package org.javamaster.httpclient.reference.support
+
+import com.intellij.ide.plugins.PluginManager
+import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.openapi.extensions.PluginId
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import org.javamaster.httpclient.psi.HttpRequestBlock
+import org.javamaster.httpclient.utils.HttpUtils
+import ris58h.webcalm.javascript.psi.*
+import java.util.*
+
+object WebCalm {
+
+    fun resolveJsVariable(variableName: String, element: PsiElement, httpFile: PsiFile): PsiElement? {
+        if (pluginNotAlive()) {
+            return null
+        }
+
+        val project = httpFile.project
+
+        val requestBlock = PsiTreeUtil.getParentOfType(element, HttpRequestBlock::class.java) ?: return null
+
+        val allPreJsScripts = HttpUtils.getAllPreJsScripts(httpFile, requestBlock).reversed()
+
+        val injectedLanguageManager = InjectedLanguageManager.getInstance(project)
+        return allPreJsScripts
+            .map {
+                val injectedPsiFiles = injectedLanguageManager.getInjectedPsiFiles(it)
+                if (injectedPsiFiles.isNullOrEmpty()) {
+                    return@map null
+                }
+
+                val jsFile = injectedPsiFiles[0].first as JavaScriptFile
+
+                val expressions = PsiTreeUtil.findChildrenOfType(jsFile, JavaScriptCallExpression::class.java)
+                for (expression in expressions) {
+                    val dotExpression =
+                        PsiTreeUtil.getChildOfType(expression, JavaScriptMemberDotExpression::class.java)
+                            ?: continue
+
+                    val arguments = PsiTreeUtil.getChildOfType(expression, JavaScriptArguments::class.java)
+                        ?: continue
+
+                    val text = dotExpression.text
+                    if (text == "request.variables.set") {
+                        return@map findArgumentName(variableName, arguments) ?: continue
+                    } else if (text == "client.global.set") {
+                        return@map findArgumentName(variableName, arguments) ?: continue
+                    }
+                }
+
+                return@map null
+            }
+            .firstOrNull { Objects.nonNull(it) }
+    }
+
+    private fun findArgumentName(variableName: String, arguments: JavaScriptArguments): JavaScriptArgument? {
+        val argumentList = PsiTreeUtil.getChildrenOfType(arguments, JavaScriptArgument::class.java)
+        return argumentList?.firstOrNull {
+            val text = it.text
+            text.substring(1, text.length - 1) == variableName
+        }
+    }
+
+    private fun pluginNotAlive(): Boolean {
+        val pluginId = PluginId.findId("ris58h.webcalm") ?: return true
+        PluginManager.getInstance().findEnabledPlugin(pluginId) ?: return true
+        return false
+    }
+
+}
