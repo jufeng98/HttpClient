b7518c86456e820bd06528d8d359d8cb5d6b0115
liangyudong <liangyudong@bluemoon.com.cn>
2025-03-25 18:09:18 +0800

    完善inspection

--- src/main/kotlin/org/javamaster/httpclient/inspection/support/InspectionHelper.kt
+++ src/main/kotlin/org/javamaster/httpclient/inspection/support/InspectionHelper.kt
@@ -1,9 +1,13 @@
 package org.javamaster.httpclient.inspection.support
 
 import com.intellij.codeInspection.InspectionManager
+import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.psi.PsiElement
+import org.javamaster.httpclient.inspection.fix.CreateEnvVariableQuickFix
+import org.javamaster.httpclient.inspection.fix.CreateFileVariableQuickFix
+import org.javamaster.httpclient.inspection.fix.CreateJsVariableQuickFix
 import org.javamaster.httpclient.reference.support.HttpVariablePsiReference
 import org.javamaster.httpclient.reference.support.JsonValueVariablePsiReference
 
@@ -17,18 +21,47 @@ object InspectionHelper {
 
         for (element in psiElements) {
             for (reference in element.references) {
-                if (reference !is HttpVariablePsiReference && reference !is JsonValueVariablePsiReference) continue
+                val variableName = when (reference) {
+                    is HttpVariablePsiReference -> {
+                        reference.variableName
+                    }
+
+                    is JsonValueVariablePsiReference -> {
+                        reference.variableName
+                    }
+
+                    else -> {
+                        null
+                    }
+                }
+
+                if (variableName == null) {
+                    continue
+                }
 
                 val resolve = reference.resolve()
 
                 if (resolve != null) continue
 
+                val fixes = mutableListOf< LocalQuickFix>(
+                    CreateEnvVariableQuickFix(false, variableName),
+                    CreateEnvVariableQuickFix(true, variableName),
+                )
+
+                if (reference is HttpVariablePsiReference) {
+                    fixes.add(CreateJsVariableQuickFix(true, variableName))
+                    fixes.add(CreateJsVariableQuickFix(false, variableName))
+                }
+
+                fixes.add(CreateFileVariableQuickFix(variableName))
+
                 val problem = manager.createProblemDescriptor(
                     element,
                     reference.rangeInElement,
-                    "Variable unresolved",
+                    "Variable $variableName unresolved",
                     ProblemHighlightType.WARNING,
                     true,
+                    *fixes.toTypedArray()
                 )
 
                 list.add(problem)
@@ -37,4 +70,5 @@ object InspectionHelper {
 
         return list.toTypedArray()
     }
+
 }
