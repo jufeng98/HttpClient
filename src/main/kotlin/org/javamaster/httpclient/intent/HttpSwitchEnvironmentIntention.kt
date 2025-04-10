5c18e1cb00bdf651f1a8aa78ee210ac3351fdf4f
liangyudong <liangyudong@bluemoon.com.cn>
2025-04-10 17:23:29 +0800

    新增intention

---
+++ src/main/kotlin/org/javamaster/httpclient/intent/HttpSwitchEnvironmentIntention.kt
@@ -0,0 +1,43 @@
+package org.javamaster.httpclient.intent
+
+import com.intellij.codeInsight.intention.impl.BaseIntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.intellij.ui.popup.PopupFactoryImpl
+import org.javamaster.httpclient.env.EnvFileService.Companion.getService
+import org.javamaster.httpclient.parser.HttpFile
+import org.javamaster.httpclient.ui.HttpEditorTopForm
+
+/**
+ * @author yudong
+ */
+class HttpSwitchEnvironmentIntention : BaseIntentionAction() {
+
+    override fun getFamilyName(): String {
+        return text
+    }
+
+    override fun getText(): String {
+        return "Switch environment"
+    }
+
+    override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
+        return file is HttpFile
+    }
+
+    override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
+        val topForm = HttpEditorTopForm.getSelectedEditorTopForm(project) ?: return
+
+        val envFileService = getService(project)
+        val path = file?.virtualFile?.parent?.path ?: return
+
+        val envSet = envFileService.getPresetEnvSet(path)
+
+        val popupFactory = PopupFactoryImpl.getInstance()
+        popupFactory.createPopupChooserBuilder(envSet.toList())
+            .setItemChosenCallback { item -> topForm.setSelectEnv(item) }
+            .createPopup()
+            .showInBestPositionFor(editor!!)
+    }
+}
