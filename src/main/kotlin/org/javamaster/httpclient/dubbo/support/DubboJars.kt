ec10bd9c58a1a9e6d2b8962a2b312b5da4a22d72
jufeng98 <375709770@qq.com>
2025-04-15 17:35:12 +0800

    完善dubbo加载逻辑

--- src/main/kotlin/org/javamaster/httpclient/dubbo/support/DubboJars.kt
+++ src/main/kotlin/org/javamaster/httpclient/dubbo/support/DubboJars.kt
@@ -9,11 +9,12 @@ import com.intellij.util.application
 import org.javamaster.httpclient.dubbo.DubboRequest
 import org.javamaster.httpclient.dubbo.loader.DubboClassLoader
 import org.javamaster.httpclient.utils.NotifyUtil
+import org.javamaster.httpclient.utils.RandomStringUtils
 import org.javamaster.httpclient.utils.StreamUtils
 import java.io.File
+import java.io.InputStream
 import java.net.URL
 import java.nio.file.Files
-import java.util.*
 import java.util.concurrent.TimeUnit
 
 /**
@@ -22,11 +23,12 @@ import java.util.concurrent.TimeUnit
 object DubboJars {
     var dubboClassLoader: DubboClassLoader
 
+    private var downloading = false
+
     private const val REPOSITORY_URL = "https://maven.aliyun.com/nexus/content/groups/public"
 
     private val jarUrls = mutableListOf<URL>()
     private val jarMap = mutableMapOf<String, URL>()
-    private val random = Random()
 
     init {
         jarUrls.addAll(findPluginJarUrls())
@@ -53,72 +55,56 @@ object DubboJars {
             URL("$REPOSITORY_URL/org/apache/zookeeper/zookeeper/3.5.3-beta/zookeeper-3.5.3-beta.jar")
     }
 
-    fun jarsDownloaded(): Boolean {
-        return jarUrls.size == jarMap.size + 2
+    fun jarsNotDownloaded(): Boolean {
+        return jarUrls.size != jarMap.size + 2
     }
 
     fun downloadAsync(project: Project) {
+        if (downloading) {
+            NotifyUtil.notifyCornerWarn(project, "Download not finished yet!")
+            return
+        }
+
+        downloading = true
+
+        NotifyUtil.notifyCornerSuccess(
+            project,
+            "Dubbo dependencies not loaded yet, Start downloading required dependencies. When finished, please try again."
+        )
+
         object : Task.Backgroundable(project, "Downloading dubbo dependencies...", true) {
             override fun run(indicator: ProgressIndicator) {
-                val dubboLibPath = getDubboLibPath()
-                if (!dubboLibPath.exists()) {
-                    dubboLibPath.mkdirs()
-                }
+                try {
+                    val dubboLibPath = getDubboLibPath()
 
-                jarUrls.clear()
-
-                var errorMsg: String? = null
-                val faction = 1.0 / jarMap.size
-
-                for ((index, entry) in jarMap.entries.withIndex()) {
-                    val name = entry.key
-                    val url = entry.value
-
-                    if (errorMsg != null) {
-                        break
+                    if (!dubboLibPath.exists()) {
+                        dubboLibPath.mkdirs()
                     }
 
-                    url.openStream()
-                        .use {
-                            try {
+                    jarUrls.clear()
+
+                    val faction = 1.0 / jarMap.size
+
+                    for ((index, entry) in jarMap.entries.withIndex()) {
+                        val name = entry.key
+                        val url = entry.value
+
+                        url.openStream()
+                            .use {
                                 if (indicator.isCanceled) {
-                                    errorMsg = "Download aborted!"
-                                    return@use
+                                    throw RuntimeException("Download aborted!")
                                 }
 
-                                val byteArray = StreamUtils.copyToByteArray(it)
-
-                                val file = File(dubboLibPath, name)
-
-                                if (file.exists()) {
-                                    file.delete()
-                                    println("deleted exists jar file: $file")
-                                }
-
-                                Files.write(file.toPath(), byteArray)
+                                val file = saveToFile(it, name, dubboLibPath)
 
                                 jarUrls.add(file.toURI().toURL())
 
                                 indicator.fraction = (index + 1) * faction
 
-                                println("Downloaded dubbo jar $name : $file")
-                            } catch (e: Exception) {
-                                errorMsg =
-                                    "Downloaded dubbo dependencies error, please try again. url: $url, error msg: ${e.message}"
-                                e.printStackTrace()
+                                TimeUnit.MILLISECONDS.sleep(1000 + RandomStringUtils.RANDOM.nextLong(2000))
                             }
-
-                            TimeUnit.MILLISECONDS.sleep(1000 + random.nextLong(2000))
-                        }
-                }
-
-                indicator.fraction = 1.0
-
-                if (errorMsg != null) {
-                    application.invokeLater {
-                        NotifyUtil.notifyCornerWarn(project, errorMsg!!)
                     }
-                } else {
+
                     dubboClassLoader.close()
 
                     jarUrls.addAll(findPluginJarUrls())
@@ -126,11 +112,43 @@ object DubboJars {
                     dubboClassLoader = DubboClassLoader(jarUrls.toTypedArray(), DubboRequest::class.java.classLoader)
 
                     application.invokeLater {
-                        NotifyUtil.notifyCornerSuccess(project, "Dubbo dependencies have been successfully downloaded!")
+                        NotifyUtil.notifyCornerSuccess(
+                            project,
+                            "Dubbo dependencies have been successfully downloaded!"
+                        )
                     }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+
+                    application.invokeLater {
+                        NotifyUtil.notifyCornerWarn(
+                            project,
+                            "Downloaded dubbo dependencies error, please try again, error msg: ${e.message}"
+                        )
+                    }
+                } finally {
+                    downloading = false
                 }
+
             }
         }.queue()
+    }
+
+    private fun saveToFile(inputStream: InputStream, name: String, dubboLibPath: File): File {
+        val byteArray = StreamUtils.copyToByteArray(inputStream)
+
+        val file = File(dubboLibPath, name)
+
+        if (file.exists()) {
+            file.delete()
+            println("deleted exists jar file: $file")
+        }
+
+        Files.write(file.toPath(), byteArray)
+
+        println("Downloaded dubbo jar $name : $file")
+
+        return file
     }
 
     private fun findPluginJarUrls(): List<URL> {
