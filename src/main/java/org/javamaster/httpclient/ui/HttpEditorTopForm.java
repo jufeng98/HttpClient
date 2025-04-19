ac628d5b72deefb94a2496450edc236ca3b8e215
jufeng98 <375709770@qq.com>
2025-04-19 09:07:11 +0800

    完善代码

--- src/main/java/org/javamaster/httpclient/ui/HttpEditorTopForm.java
+++ src/main/java/org/javamaster/httpclient/ui/HttpEditorTopForm.java
@@ -1,32 +1,37 @@
 package org.javamaster.httpclient.ui;
 
 import com.google.common.collect.Maps;
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.actionSystem.ActionGroup;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.ui.popup.ListPopup;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.awt.RelativePoint;
+import com.intellij.ui.popup.PopupFactoryImpl;
 import kotlin.Triple;
 import org.apache.commons.compress.utils.Lists;
 import org.javamaster.httpclient.HttpIcons;
-import org.javamaster.httpclient.env.EnvFileService;
 import org.javamaster.httpclient.handler.RunFileHandler;
-import org.javamaster.httpclient.utils.NotifyUtil;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
-import static org.javamaster.httpclient.env.EnvFileService.ENV_FILE_NAME;
-import static org.javamaster.httpclient.env.EnvFileService.PRIVATE_ENV_FILE_NAME;
 import static org.javamaster.httpclient.nls.NlsBundle.INSTANCE;
 
 /**
@@ -41,26 +46,22 @@ public class HttpEditorTopForm extends JComponent {
     {
         options.add(INSTANCE.nls("examples.and.environments"));
         optionIndexMap.put(0, null);
-        options.add(INSTANCE.nls("create.env.json.file"));
-        optionIndexMap.put(1, ENV_FILE_NAME);
-        options.add(INSTANCE.nls("create.env.private.json.file"));
-        optionIndexMap.put(2, PRIVATE_ENV_FILE_NAME);
         options.add(INSTANCE.nls("get.requests"));
-        optionIndexMap.put(3, "examples/get-requests.http");
+        optionIndexMap.put(1, "examples/get-requests.http");
         options.add(INSTANCE.nls("post.requests"));
-        optionIndexMap.put(4, "examples/post-requests.http");
+        optionIndexMap.put(2, "examples/post-requests.http");
         options.add(INSTANCE.nls("request.with.authorization"));
-        optionIndexMap.put(5, "examples/requests-with-authorization.http");
+        optionIndexMap.put(3, "examples/requests-with-authorization.http");
         options.add(INSTANCE.nls("request.with.tests.and.scripts"));
-        optionIndexMap.put(6, "examples/requests-with-scripts.http");
+        optionIndexMap.put(4, "examples/requests-with-scripts.http");
         options.add(INSTANCE.nls("response.presentations"));
-        optionIndexMap.put(7, "examples/responses-presentation.http");
+        optionIndexMap.put(5, "examples/responses-presentation.http");
         options.add(INSTANCE.nls("websocket.requests"));
-        optionIndexMap.put(8, "examples/ws-requests.http");
+        optionIndexMap.put(6, "examples/ws-requests.http");
         options.add(INSTANCE.nls("dubbo.requests"));
-        optionIndexMap.put(9, "examples/dubbo-requests.http");
+        optionIndexMap.put(7, "examples/dubbo-requests.http");
         options.add(INSTANCE.nls("show.cryptojs.file"));
-        optionIndexMap.put(10, "examples/crypto-js.js");
+        optionIndexMap.put(8, "examples/crypto-js.js");
     }
 
     public final VirtualFile file;
@@ -69,14 +70,30 @@ public class HttpEditorTopForm extends JComponent {
     private JComboBox<String> exampleComboBox;
     private JButton showVariableBtn;
     private JButton runAllBtn;
+    private JButton addBtn;
 
     private final @Nullable Module module;
-    private final Project project;
 
     public HttpEditorTopForm(VirtualFile file, @Nullable Module module, Project project) {
         this.file = file;
         this.module = module;
-        this.project = project;
+
+        addBtn.setIcon(AllIcons.General.Add);
+        addBtn.setBorder(null);
+        addBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                ActionManager actionManager = ActionManager.getInstance();
+                ActionGroup group = (ActionGroup) actionManager.getAction("addToHttp");
+
+                JBPopupFactory popupFactory = PopupFactoryImpl.getInstance();
+                ListPopup listPopup = popupFactory.createActionGroupPopup(INSTANCE.nls("new"), group,
+                        DataContext.EMPTY_CONTEXT, true, null, 10);
+
+                Point point = e.getPoint();
+                listPopup.show(new RelativePoint(e.getComponent(), new Point(point.x - 20, point.y + 20)));
+            }
+        });
 
         DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>();
         model.addAll(options);
@@ -110,17 +127,10 @@ public class HttpEditorTopForm extends JComponent {
                 return;
             }
 
-            if (ENV_FILE_NAME.equals(option)) {
-                createAndReInitEnvCompo(false);
-            } else if (PRIVATE_ENV_FILE_NAME.equals(option)) {
-                createAndReInitEnvCompo(true);
-            } else {
-                URL url = classLoader.getResource(option);
-                //noinspection DataFlowIssue
-                VirtualFile virtualFile = VfsUtil.findFileByURL(url);
-                //noinspection DataFlowIssue
-                FileEditorManager.getInstance(project).openFile(virtualFile, true);
-            }
+            URL url = classLoader.getResource(option);
+            VirtualFile virtualFile = VfsUtil.findFileByURL(url);
+            //noinspection DataFlowIssue
+            FileEditorManager.getInstance(project).openFile(virtualFile, true);
         });
 
         showVariableBtn.addActionListener(e -> {
@@ -137,41 +147,6 @@ public class HttpEditorTopForm extends JComponent {
     public void switchRunBtnToStopping() {
         runAllBtn.setToolTipText(INSTANCE.nls("stop.running"));
         runAllBtn.setIcon(HttpIcons.STOP);
-    }
-
-    public void createAndReInitEnvCompo(boolean isPrivate) {
-        String envFileName = isPrivate ? PRIVATE_ENV_FILE_NAME : ENV_FILE_NAME;
-
-        VirtualFile envFile = EnvFileService.Companion.createEnvFile(envFileName, isPrivate, project);
-        exampleComboBox.setSelectedIndex(0);
-        if (envFile == null) {
-            NotifyUtil.INSTANCE.notifyWarn(project, envFileName + " " + INSTANCE.nls("file.exists"));
-            return;
-        }
-
-        FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
-        fileEditorManager.openFile(envFile, true);
-
-        NotifyUtil.INSTANCE.notifyInfo(project, INSTANCE.nls("file.created") + " " + envFileName);
-
-        try {
-            FileEditor @NotNull [] allEditors = fileEditorManager.getAllEditors();
-            for (FileEditor editor : allEditors) {
-                HttpEditorTopForm httpEditorTopForm = editor.getUserData(HttpEditorTopForm.KEY);
-                if (httpEditorTopForm == null) {
-                    continue;
-                }
-
-                LinkedHashSet<String> set = new LinkedHashSet<>();
-                set.add("dev");
-                set.add("uat");
-                set.add("pro");
-                httpEditorTopForm.initEnvCombo(set);
-            }
-        } catch (Exception e) {
-            //noinspection CallToPrintStackTrace
-            e.printStackTrace();
-        }
     }
 
     public void initEnvCombo(Set<String> presetEnvSet) {
