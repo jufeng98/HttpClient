14ee9a609679c0a89b4346f3ebfd44d2a20875c2
jufeng98 <375709770@qq.com>
2025-04-21 17:33:55 +0800

    完善测试类

--- src/test/kotlin/org/javamaster/test/GitTest.kt
+++ src/test/kotlin/org/javamaster/test/GitTest.kt
@@ -4,6 +4,7 @@ import org.junit.Test
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
+import java.util.regex.Pattern
 
 class GitTest {
 
@@ -11,6 +12,10 @@ class GitTest {
     fun push() {
         val file = File("")
         println(file.absolutePath)
+
+        val proxyAddress = getProxyAddress()
+
+        execCommand("cmd /c git config http.proxy $proxyAddress")
 
         execCommand("cmd /c git push")
     }
@@ -20,10 +25,29 @@ class GitTest {
         val file = File("")
         println(file.absolutePath)
 
-        execCommand("cmd /c git pull")
+        val proxyAddress = getProxyAddress()
+
+        execCommand("cmd", "/c", "git config --global http.proxy $proxyAddress")
+
+        execCommand("cmd", "/c", "git pull")
     }
 
-    private fun execCommand(command: String) {
+    private fun getProxyAddress(): String {
+        val regKey = "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings"
+        val res = execCommand("reg", "query", regKey)
+
+        val split = res.split("\r\n")
+        for (s in split) {
+            if (s.contains("ProxyServer")) {
+                val strings = s.split(Pattern.compile("\\s+"))
+                return strings.last()
+            }
+        }
+
+        throw IllegalArgumentException()
+    }
+
+    private fun execCommand(vararg command: String): String {
         val process = Runtime.getRuntime().exec(command)
         process.waitFor()
 
@@ -31,20 +55,32 @@ class GitTest {
         println("result:$res")
 
         if (res.contains("fatal")) {
-            execCommand(command)
+            return execCommand(*command)
         }
+
+        return res
     }
 
     private fun getRes(process: Process): String {
         val stringBuilder = StringBuilder()
 
-        BufferedReader(InputStreamReader(process.errorStream)).use {
-            var line = it.readLine()
-            while (line != null) {
-                stringBuilder.append(line)
-                line = it.readLine()
+        BufferedReader(InputStreamReader(process.inputStream))
+            .use {
+                var line = it.readLine()
+                while (line != null) {
+                    stringBuilder.append(line + "\r\n")
+                    line = it.readLine()
+                }
             }
-            return stringBuilder.toString()
-        }
+
+        BufferedReader(InputStreamReader(process.errorStream))
+            .use {
+                var line = it.readLine()
+                while (line != null) {
+                    stringBuilder.append(line + "\r\n")
+                    line = it.readLine()
+                }
+                return stringBuilder.toString()
+            }
     }
 }
