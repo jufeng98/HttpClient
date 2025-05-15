c0bc8c3c8769895141faabd945229f2f1818f312
jufeng98 <375709770@qq.com>
2025-05-15 20:50:32 +0800

    完善说明

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "org.javamaster"
-version = "3.9.8"
+version = "5.0.0"
 
 repositories {
     maven { url = URI("https://maven.aliyun.com/nexus/content/groups/public/") }
--- src/main/kotlin/org/javamaster/httpclient/action/dashboard/ViewSettingsAction.kt
+++ src/main/kotlin/org/javamaster/httpclient/action/dashboard/ViewSettingsAction.kt
@@ -1,12 +1,12 @@
-package org.javamaster.httpclient.action.dashboard;
+package org.javamaster.httpclient.action.dashboard
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 
-public class ViewSettingsAction extends AnAction {
-    @Override
-    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+class ViewSettingsAction : AnAction() {
+
+    override fun actionPerformed(anActionEvent: AnActionEvent) {
 
     }
+
 }
