ac8dd9a7b9f1760ec7dc17b35dc20eb162247827
jufeng98 <375709770@qq.com>
2025-05-19 17:39:28 +0800

    完善bnf

---
+++ src/main/gen/org/javamaster/httpclient/psi/impl/HttpHistoryBodyFileListImpl.java
@@ -0,0 +1,36 @@
+// This is a generated file. Not intended for manual editing.
+package org.javamaster.httpclient.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static org.javamaster.httpclient.psi.HttpTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import org.javamaster.httpclient.psi.*;
+
+public class HttpHistoryBodyFileListImpl extends ASTWrapperPsiElement implements HttpHistoryBodyFileList {
+
+  public HttpHistoryBodyFileListImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull HttpVisitor visitor) {
+    visitor.visitHistoryBodyFileList(this);
+  }
+
+  @Override
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof HttpVisitor) accept((HttpVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public List<HttpHistoryBodyFile> getHistoryBodyFileList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, HttpHistoryBodyFile.class);
+  }
+
+}
