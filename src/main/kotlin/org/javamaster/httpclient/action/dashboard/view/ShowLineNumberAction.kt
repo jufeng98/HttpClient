b3353e0d9136001680603d23c24232deaadbb8cc
jufeng98 <375709770@qq.com>
2025-05-20 17:33:29 +0800

    完善显示行号动作

--- src/main/kotlin/org/javamaster/httpclient/action/dashboard/view/ShowLineNumberAction.kt
+++ src/main/kotlin/org/javamaster/httpclient/action/dashboard/view/ShowLineNumberAction.kt
@@ -2,48 +2,65 @@ package org.javamaster.httpclient.action.dashboard.view
 
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.openapi.actionSystem.ex.CustomComponentAction
+import com.intellij.openapi.actionSystem.impl.ActionButtonWithText
 import com.intellij.openapi.editor.Editor
+import org.javamaster.httpclient.HttpIcons
 import org.javamaster.httpclient.action.dashboard.DashboardBaseAction
 import org.javamaster.httpclient.nls.NlsBundle.nls
+import java.awt.Dimension
+import javax.swing.JComponent
 
 /**
  * @author yudong
  */
 class ShowLineNumberAction(private val editor: Editor, private val req: Boolean) :
-    DashboardBaseAction(nls("show.line.num"), null) {
-    init {
-        val showLineNum = if (req) {
+    DashboardBaseAction(nls("show.line.num"), null), CustomComponentAction {
+
+    override fun createCustomComponent(presentation: Presentation, place: String): JComponent {
+        val actionButtonWithText = ActionButtonWithText(this, presentation, place, Dimension(20, 20))
+
+        val showLineNum = getShowLineNum()
+
+        setShowLineNum(showLineNum, actionButtonWithText)
+
+        return actionButtonWithText
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val actionButtonWithText = e.inputEvent!!.component as ActionButtonWithText
+
+        val showLineNum = switchShowLineNum()
+
+        setShowLineNum(showLineNum, actionButtonWithText)
+    }
+
+    private fun setShowLineNum(showLineNum: Boolean, actionButtonWithText: ActionButtonWithText) {
+        editor.settings.isLineNumbersShown = showLineNum
+
+        if (showLineNum) {
+            actionButtonWithText.presentation.icon = AllIcons.Actions.Checked
+        } else {
+            actionButtonWithText.presentation.icon = HttpIcons.BLANK
+        }
+    }
+
+    private fun getShowLineNum(): Boolean {
+        return if (req) {
             reqShowLineNum
         } else {
             resShowLineNum
         }
-
-        if (showLineNum) {
-            templatePresentation.icon = AllIcons.Actions.Checked
-        } else {
-            templatePresentation.icon = null
-        }
     }
 
-    override fun actionPerformed(e: AnActionEvent) {
-        val showLineNum = if (req) {
+    private fun switchShowLineNum(): Boolean {
+        return if (req) {
             reqShowLineNum = !reqShowLineNum
             reqShowLineNum
         } else {
             resShowLineNum = !resShowLineNum
             resShowLineNum
-        }
-
-        setShowLineNum(showLineNum)
-    }
-
-    private fun setShowLineNum(showLineNum: Boolean) {
-        editor.settings.isLineNumbersShown = showLineNum
-
-        if (showLineNum) {
-            templatePresentation.icon = AllIcons.Actions.Checked
-        } else {
-            templatePresentation.icon = null
         }
     }
 
