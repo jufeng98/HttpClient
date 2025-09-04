5af6db9dfb469c8f8479887f09629b2364484763
jufeng98 <375709770@qq.com>
2025-09-04 22:00:58 +0800

    调整测试类

--- src/test/kotlin/org/javamaster/test/GitTest.kt
+++ src/test/kotlin/org/javamaster/test/GitTest.kt
@@ -4,7 +4,6 @@ import org.junit.Test
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
-import java.util.regex.Pattern
 
 class GitTest {
 
@@ -12,10 +11,6 @@ class GitTest {
     fun push() {
         val file = File("")
         println(file.absolutePath)
-
-        val proxyAddress = getProxyAddress()
-
-        execCommand("cmd", "/c", "git config http.proxy $proxyAddress")
 
         execCommand("cmd", "/c", "git push")
     }
@@ -25,26 +20,7 @@ class GitTest {
         val file = File("")
         println(file.absolutePath)
 
-        val proxyAddress = getProxyAddress()
-
-        execCommand("cmd", "/c", "git config http.proxy $proxyAddress")
-
         execCommand("cmd", "/c", "git pull")
-    }
-
-    private fun getProxyAddress(): String {
-        val regKey = "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings"
-        val res = execCommand("reg", "query", regKey)
-
-        val split = res.split("\r\n")
-        for (s in split) {
-            if (s.contains("ProxyServer")) {
-                val strings = s.split(Pattern.compile("\\s+"))
-                return strings.last()
-            }
-        }
-
-        throw IllegalArgumentException()
     }
 
     private fun execCommand(vararg command: String): String {
