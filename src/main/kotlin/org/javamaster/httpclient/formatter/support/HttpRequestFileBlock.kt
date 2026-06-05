d7c79790104a76f57b423b233077704307102419
jufeng98 <375709770@qq.com>
2026-06-05 18:14:35 +0800

    调整格式化

--- src/main/kotlin/org/javamaster/httpclient/formatter/support/HttpRequestFileBlock.kt
+++ src/main/kotlin/org/javamaster/httpclient/formatter/support/HttpRequestFileBlock.kt
@@ -4,11 +4,8 @@ import com.intellij.formatting.Block
 import com.intellij.formatting.Spacing
 import com.intellij.formatting.Wrap
 import com.intellij.lang.ASTNode
-import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.codeStyle.CodeStyleSettings
 import com.intellij.psi.formatter.common.SettingsAwareBlock
-import com.intellij.psi.util.PsiTreeUtil
-import org.apache.commons.lang3.StringUtils
 import org.javamaster.httpclient.psi.HttpTypes
 
 /**
@@ -37,26 +34,7 @@ class HttpRequestFileBlock(fileNode: ASTNode, private val mySettings: CodeStyleS
             return Spacing.getReadOnlySpacing()
         }
 
-        val psiElement1 = child1.node.psi
-        val nextSibling = psiElement1.nextSibling
-        if (nextSibling is PsiWhiteSpace) {
-            val text = nextSibling.text
-            val lineFeeds = StringUtils.countMatches(text, "\n")
-            return if (lineFeeds == 1) {
-                Spacing.createSpacing(0, 0, 2, true, 100)
-            } else {
-                Spacing.getReadOnlySpacing()
-            }
-        }
-
-        val lastLeaf = PsiTreeUtil.lastChild(psiElement1)
-        val text = lastLeaf.text
-        val length = text.length
-        return if (length > 1 && text[length - 1] == '\n' && !text[length - 2].isWhitespace()) {
-            Spacing.createSpacing(0, 0, 1, true, 100)
-        } else {
-            Spacing.getReadOnlySpacing()
-        }
+        return Spacing.createSpacing(0, 0, 2, false, 1)
     }
 
     override fun isLeaf(): Boolean {
