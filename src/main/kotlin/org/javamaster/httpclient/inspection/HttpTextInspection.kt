4acfb2179f8b4017d8ffc16c7eaee804f1cfc74a
jufeng98 <375709770@qq.com>
2026-06-05 18:06:31 +0800

    完善inspection

--- src/main/kotlin/org/javamaster/httpclient/inspection/HttpTextInspection.kt
+++ src/main/kotlin/org/javamaster/httpclient/inspection/HttpTextInspection.kt
@@ -3,15 +3,22 @@ package org.javamaster.httpclient.inspection
 import com.intellij.codeInspection.InspectionManager
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.lang.injection.InjectedLanguageManager
 import com.intellij.psi.PsiFile
 import org.javamaster.httpclient.inspection.support.InspectionHelper
+import org.javamaster.httpclient.parser.HttpFile
 
 /**
  * @author yudong
  */
-class MyTextInspection : LocalInspectionTool() {
+class HttpTextInspection : LocalInspectionTool() {
 
     override fun checkFile(file: PsiFile, manager: InspectionManager, isOnTheFly: Boolean): Array<ProblemDescriptor> {
+        val topLevelFile = InjectedLanguageManager.getInstance(file.project).getTopLevelFile(file)
+        if (topLevelFile !is HttpFile) {
+            return emptyArray()
+        }
+
         return InspectionHelper.checkVariables(listOf(file), manager)
     }
 
