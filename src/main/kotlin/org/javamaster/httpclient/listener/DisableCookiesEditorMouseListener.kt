6743e749349a134591a36bb88ad8e938fad8979d
jufeng98 <375709770@qq.com>
2026-06-28 11:28:49 +0800

    完善cookie展示逻辑

--- src/main/kotlin/org/javamaster/httpclient/listener/DisableCookiesEditorMouseListener.kt
+++ src/main/kotlin/org/javamaster/httpclient/listener/DisableCookiesEditorMouseListener.kt
@@ -1,13 +1,11 @@
 package org.javamaster.httpclient.listener
 
-import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.EditorCustomElementRenderer
 import com.intellij.openapi.editor.Inlay
 import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.editor.event.EditorMouseListener
-import com.intellij.pom.Navigatable
 import org.javamaster.httpclient.enums.ParamEnum
-import org.javamaster.httpclient.factory.HttpPsiFactory
+import org.javamaster.httpclient.parser.HttpFile
 import org.javamaster.httpclient.psi.HttpRequestBlock
 
 /**
@@ -29,20 +27,7 @@ class DisableCookiesEditorMouseListener(
         if (bounds.contains(point)) {
             val project = requestBlock.project
 
-            WriteCommandAction.runWriteCommandAction(project) {
-                val directionComment = HttpPsiFactory.createDirectionComment(project, "# @${ParamEnum.NO_COOKIE_JAR.param}\n")
-                val nextSibling = directionComment.nextSibling
-                val comment = requestBlock.comment
-                if (comment != null) {
-                    val elementNew = requestBlock.addAfter(directionComment, comment)
-                    requestBlock.addAfter(nextSibling, elementNew)
-                    (elementNew as Navigatable).navigate(true)
-                } else {
-                    val elementNew = requestBlock.addBefore(directionComment, requestBlock.firstChild)
-                    requestBlock.addAfter(nextSibling, elementNew)
-                    (elementNew as Navigatable).navigate(true)
-                }
-            }
+            HttpFile.insertReqDirectionComment(requestBlock, ParamEnum.NO_COOKIE_JAR, project)
         }
     }
 
--- src/main/kotlin/org/javamaster/httpclient/parser/HttpFile.kt
+++ src/main/kotlin/org/javamaster/httpclient/parser/HttpFile.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Computable
+import com.intellij.pom.Navigatable
 import com.intellij.psi.FileViewProvider
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -15,6 +16,8 @@ import com.intellij.psi.util.PsiUtil
 import com.intellij.refactoring.rename.RenameProcessor
 import org.javamaster.httpclient.HttpFileType
 import org.javamaster.httpclient.HttpLanguage
+import org.javamaster.httpclient.enums.ParamEnum
+import org.javamaster.httpclient.factory.HttpPsiFactory
 import org.javamaster.httpclient.factory.HttpPsiFactory.createFileVariable
 import org.javamaster.httpclient.psi.*
 
@@ -164,6 +167,23 @@ class HttpFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, HttpL
             val textEditor = FileEditorManager.getInstance(project).selectedTextEditor ?: return null
             val httpFile = PsiUtil.getPsiFile(project, textEditor.virtualFile) as HttpFile
             return httpFile.createFileVariableAndInsert(variableName, variableValue)
+        }
+
+        fun insertReqDirectionComment(requestBlock: HttpRequestBlock, paramEnum: ParamEnum, project: Project) {
+            WriteCommandAction.runWriteCommandAction(project) {
+                val directionComment = HttpPsiFactory.createDirectionComment(project, "# @${paramEnum.param}\n")
+                val nextSibling = directionComment.nextSibling
+                val comment = requestBlock.comment
+                if (comment != null) {
+                    val elementNew = requestBlock.addAfter(directionComment, comment)
+                    requestBlock.addAfter(nextSibling, elementNew)
+                    (elementNew as Navigatable).navigate(true)
+                } else {
+                    val elementNew = requestBlock.addBefore(directionComment, requestBlock.firstChild)
+                    requestBlock.addAfter(nextSibling, elementNew)
+                    (elementNew as Navigatable).navigate(true)
+                }
+            }
         }
 
     }
